95e29bd03807674a912cb40e994b973a3db7faaa
Marc Cavazos <marcavazos19@icloud.com>
2022-02-04 21:09:26 -0500

    commit tree afaf2007c461711ece162ec4ef393928bc35b7ce

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [image, setImage] = useState('');
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            setImage(data)
+            setIsLoaded(true);
+        })
+    }, [])
+
+    if (!isLoaded) {
+        return <h3>Loading...</h3>;
+    }
+
+
+  return (
+    <div>
+        <img src={image.message} alt='A Random Dog'/>
+    </div>
+    );
+}
+
+export default App;
